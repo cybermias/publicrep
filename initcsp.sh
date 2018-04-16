4c144255c4c6988831185c718f22e293d8255f73
nadavna <37038484+nadavna@users.noreply.github.com>
2018-04-16 21:23:08 +0300

    Update initcsp.sh

--- initcsp.sh
+++ initcsp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "net.ipv4.ip_forward = 1" > /etc/sysctl.conf
+sudo sysctl -p /etc/sysctl.conf
 
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
