5554947522bae064e393435a7a264536ba8d664b
nadavna <37038484+nadavna@users.noreply.github.com>
2018-04-18 19:36:39 +0300

    Create kali-init.sh

---
+++ kali-init.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+sudo wget -q -O - https://archive.kali.org/archive-key.asc | apt-key add
+sudo apt-get update
+
+sudo apt install -y xrdp tigervnc-standalone-server
+
+cd /etc/xrdp
+
+cat <<EOF | sudo patch -p1 -f -s
+--- a/xrdp.ini     2017-06-19 14:05:53.290490260 +0900
++++ b/xrdp.ini  2017-06-19 14:11:17.788557402 +0900
+@@ -147,15 +147,6 @@ tcutils=true
+ ; Session types
+ ;
+
+-[Xorg]
+-name=Xorg
+-lib=libxup.so
+-username=ask
+-password=ask
+-ip=127.0.0.1
+-port=-1
+-code=20
+-
+ [Xvnc]
+ name=Xvnc
+ lib=libvnc.so
+@@ -166,6 +157,15 @@ port=-1
+ #xserverbpp=24
+ #delay_ms=2000
+
++[Xorg]
++name=Xorg
++lib=libxup.so
++username=ask
++password=ask
++ip=127.0.0.1
++port=-1
++code=20
++
+ [console]
+ name=console
+ lib=libvnc.so
+
+EOF
+
+sudo systemctl enable xrdp
+sudo systemctl restart xrdp
