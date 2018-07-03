29ef2b285bc6e592e39ac85592cd9accb35bf581
nadavna <37038484+nadavna@users.noreply.github.com>
2018-07-03 09:41:02 +0300

    Create OMpendock.sh
    For pentesting with Docker (OM)

---
+++ area51/OMpendock.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+cd ~/Desktop
+sudo apt-get -y install git
+git clone https://github.com/eystsen/pentestlab
+cd pentestlab/
+ 
+if fuser /var/lib/dpkg/lock > /dev/null 2>&1
+then
+        echo "Software management (APT) is already running. Cannot install docker right now."
+        echo "Just wait a minute or two and try again"
+        exit
+fi
+ 
+ 
+sudo apt-get update
+ 
+# apt-get dependencies
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+ 
+# Add gpg key for docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+ 
+echo Adding to sources.list
+echo "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable" | sudo tee --append /etc/apt/sources.list > /dev/null
+ 
+ 
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
+ 
+# Start Docker on boot
+sudo systemctl enable docker
+cd Desktop/pentestlab/
+
+sudo ./pentestlab.sh start bwapp && sudo ./pentestlab.sh start webgoat8 && sudo ./pentestlab.sh start dvwa
