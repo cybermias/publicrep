b00c84ee19486f4e5de4b7f2732533b43ffeaf30
nadavna <37038484+nadavna@users.noreply.github.com>
2018-07-17 23:57:40 +0300

    Update vagrant-init.sh

--- vagrants/vagrant-init.sh
+++ vagrants/vagrant-init.sh
@@ -6,6 +6,8 @@ apt-add-repository ppa:ansible/ansible
 apt-get update && sudo apt-get install -y ansible
 
 sudo apt-get install -y ruby-dev zlib1g-dev liblzma-dev build-essential patch virtualbox ruby-bundler imagemagick libmagickwand-dev libpq-dev libcurl4-openssl-dev libxml2-dev
+echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections
+sudo apt-get install -y virtualbox-ext-pack
 
 sudo apt-get -y install apache2 libapache2-mod-php7.0 libapr1 libaprutil1 libaprutil1-dbd-sqlite3 libaprutil1-ldap libapr1 php7.0-common php7.0-mysql php7.0-soap php-pear wget
 
@@ -32,9 +34,11 @@ cd /opt
 #unzip IE10.Win7.VirtualBox.zip
 #rm IE10.Win7.VirtualBox.zip
 
-vagrant init nwedlake/Windows7x64
+wget https://raw.githubusercontent.com/cybermias/publicrep/master/vagrants/Vagrantfile
 vagrant up
 
+echo "cd /opt && vagrant up" > /etc/rc.local
+echo "exit 0" >> /etc/rc.local
 
 #vboxmanage import "IE10 - Win7.ova" --vsys 0 --memory 2048 --cpus 1 --vmname "win7ie10"
 #vboxmanage modifyvm "win7ie10" --vrde on --vrdeport 3389 --vrdeaddress 0.0.0.0
