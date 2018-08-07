a5a007b538567abc5dee1cb68f2e019613a4dda4
nadavna <37038484+nadavna@users.noreply.github.com>
2018-08-07 17:26:08 +0300

    Update init-ovpn-snapshot.sh

--- Infrastructure/openvpn/init-ovpn-snapshot.sh
+++ Infrastructure/openvpn/init-ovpn-snapshot.sh
@@ -31,7 +31,7 @@ sudo systemctl restart openvpnas
 
 #Remember to verify IP with every boot (for dynamic IP)
 sudo echo '#!/bin/bash' > /etc/init.d/fixvpnpip.sh
-sudo echo 'vnet="$(/sbin/ip -o -4 addr list eth0 | awk '{print \$4}' | cut -d. -f1-2).0.0/16"' >> /etc/init.d/fixvpnpip.sh
+sudo echo 'vnet="$(/sbin/ip -o -4 addr list eth0 | awk '"'"'{print $4}'"'"' | cut -d. -f1-2).0.0/16"' >> /etc/init.d/fixvpnpip.sh
 sudo echo 'PUBLICIP=$(curl -s ipecho.net/plain)' >> /etc/init.d/fixvpnpip.sh
 sudo echo 'while [ ! $PUBLICIP ]; do' >> /etc/init.d/fixvpnpip.sh
 sudo echo '        PUBLICIP=$(curl -s ipecho.net/plain)' >> /etc/init.d/fixvpnpip.sh
