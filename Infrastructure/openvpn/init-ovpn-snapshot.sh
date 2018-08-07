5d1d95009b9920f697a95b5ef9d04517473a9170
nadavna <37038484+nadavna@users.noreply.github.com>
2018-08-07 16:30:17 +0300

    Update init-ovpn-snapshot.sh

--- Infrastructure/openvpn/init-ovpn-snapshot.sh
+++ Infrastructure/openvpn/init-ovpn-snapshot.sh
@@ -35,6 +35,7 @@ sudo echo 'PUBLICIP=$(curl -s ipecho.net/plain)' >> /etc/init.d/fixvpnpip.sh
 sudo echo 'while [ ! $PUBLICIP ]; do' >> /etc/init.d/fixvpnpip.sh
 sudo echo '        PUBLICIP=$(curl -s ipecho.net/plain)' >> /etc/init.d/fixvpnpip.sh
 sudo echo 'done' >> /etc/init.d/fixvpnpip.sh
+sudo echo 'sudo sqlite3 "/usr/local/openvpn_as/etc/db/config.db" "update config set value='"'"'$vnetfix'"'"' where name='"'"'vpn.server.routing.private_network.0'"'"';"' >> /etc/init.d/fixvpnpip.sh
 sudo echo 'sudo sqlite3 "/usr/local/openvpn_as/etc/db/config.db" "update config set value='"'"'$PUBLICIP'"'"' where name='"'"'host.name'"'"';"' >> /etc/init.d/fixvpnpip.sh
 chmod ugo+x /etc/init.d/fixvpnpip.sh
 sudo update-rc.d fixvpnpip.sh defaults
