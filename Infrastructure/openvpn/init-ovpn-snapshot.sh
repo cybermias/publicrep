65e1ee1de4e0c412f166bdcd38081c453b134fbe
nadavna <37038484+nadavna@users.noreply.github.com>
2018-08-10 21:43:30 +0300

    Update init-ovpn-snapshot.sh

--- Infrastructure/openvpn/init-ovpn-snapshot.sh
+++ Infrastructure/openvpn/init-ovpn-snapshot.sh
@@ -4,8 +4,8 @@
 
 # Script accepts userPassword for openvpn and must either recieve (or calculate) VNET with CIDR prefix - In this version VNET is calculated (SNAPSHOTs)
 #userPassword=$1
-vnet=$1
-#vnet="$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d. -f1-2).0.0/16"
+#vnet=$1
+vnet="$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d. -f1-2).0.0/16"
 
 # Packages were already downloaded as per snapshot
 
