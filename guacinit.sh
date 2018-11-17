2800ed4b54245fc8745c2985a6ab50e3aac6c9ca
nadavna <37038484+nadavna@users.noreply.github.com>
2018-11-17 23:07:39 +0200

    Create guacinit.sh

---
+++ guacinit.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+cat <<EOF > /etc/guacamole/guacamole.properties
+# Hostname and port of guacamole proxy
+guacd-hostname: localhost
+guacd-port:     4822
+
+# MySQL properties
+#mysql-hostname: localhost
+#mysql-port: 3306
+#mysql-database: cmtsguac
+#mysql-username: cmtsadmin
+#mysql-password: cmtsAdmin12#
+#mysql-default-max-connections-per-user: 0
+#mysql-default-max-group-connections-per-user: 0
+
+auth-provider: net.sourceforge.guacamole.net.basic.BasicFileAuthenticationProvider
+basic-user-mapping: /etc/guacamole/user-mapping.xml
+EOF
+
+cat <<EOF > /etc/guacamole/user-mapping.xml
+<user-mapping>
+    <authorize username="cmtsadmin" password="cmtsAdmin12#">
+        <connection name="WIN-DESKTOP">
+            <protocol>rdp</protocol>
+            <param name="hostname">10.200.11.10</param>
+            <param name="port">3389</param>
+            <param name="username">cmtsadmin</param>
+            <param name="password">cmtsAdmin12#</param>
+            <param name="ignore-cert">true</param>
+            <param name="security">any</param>
+        </connection>
+
+        <!-- Second authorized connection -->
+        <connection name="KALI">
+            <protocol>ssh</protocol>
+            <param name="hostname">10.200.1.10</param>
+            <param name="port">22</param>
+            <param name="username">cmtsadmin</param>
+            <param name="password">cmtsAdmin12#</param>
+        </connection>
+
+    </authorize>
+</user-mapping>
+EOF
+
+service tomcat restart
+service nginx restart
+service guacd restart
