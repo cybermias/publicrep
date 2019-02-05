038f46540533ff4bb69fa0bea81c3fba41572694
nadavna <37038484+nadavna@users.noreply.github.com>
2019-02-05 20:57:14 +0200

    Update clientprovision.ps1

--- microsoft/clientprovision.ps1
+++ microsoft/clientprovision.ps1
@@ -2,3 +2,5 @@
 cscript c:\windows\system32\slmgr.vbs /rearm
 
 REG add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "PagingFiles" /t REG_MULTI_SZ /d "D:\pagefile.sys 0 0" /f
+
+shutdown /r /t 02
