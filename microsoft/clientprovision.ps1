1a06b2193285b4c72ffab2c2a78d8d5f2a6414a9
nadavna <37038484+nadavna@users.noreply.github.com>
2019-02-05 21:17:43 +0200

    Update clientprovision.ps1

--- microsoft/clientprovision.ps1
+++ microsoft/clientprovision.ps1
@@ -3,4 +3,10 @@ cscript c:\windows\system32\slmgr.vbs /rearm
 
 REG add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "PagingFiles" /t REG_MULTI_SZ /d "D:\pagefile.sys 0 0" /f
 
+wget https://github.com/cybermias/publicrep/raw/master/microsoft/digital_forensics.jpg -O c:\windows\system32\wallpaper.jpg
+
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d c:\windows\system32\wallpaper.jpg /f
+Start-Sleep -s 2
+rundll32.exe user32.dll, UpdatePerUserSystemParameters, 0, $false
+
 shutdown /r /t 02
