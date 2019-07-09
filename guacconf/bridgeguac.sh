b1ab140a25eb31ef6aa128e127d61afa90524116
nadavna <37038484+nadavna@users.noreply.github.com>
2019-07-09 23:55:12 +0300

    Update bridgeguac.sh

--- guacconf/bridgeguac.sh
+++ guacconf/bridgeguac.sh
@@ -126,16 +126,16 @@ sudo service guacd restart
 
 # ADDING OPENVPN CONFIGURATION NOT TO ALTER ORIGINAL FIXVPNIP.SH (2.7.3 needs to be upgraded, its also faulty here)
 # Added at 20190709 - Before Itzik mimikatz class at HDE
-sudo /usr/local/openvpn_as/scripts/sacli --key "vpn.daemon.0.client.network" --value "$brnet" ConfigPut
-sudo /usr/local/openvpn_as/scripts/sacli --key "vpn.daemon.0.client.netmask_bits" --value "29" ConfigPut
 
-sudo /usr/local/openvpn_as/scripts/sacli --user cmtsadmin --key "access_to.0" --value "+ROUTE:$vnet" userpropput
-sudo /usr/local/openvpn_as/scripts/sacli --user cmtsadmin --key "access_from.0" --value "+ALL_S2C_SUBNETS" userpropput
-sudo /usr/local/openvpn_as/scripts/sacli --user cmtsadmin --key "access_from.0" --value "+ALL_S2C_SUBNETS" userpropput
+sudo /usr/local/openvpn_as/scripts/sacli -k vpn.daemon.0.client.network -v $brnet ConfigPut
+sudo /usr/local/openvpn_as/scripts/sacli -k vpn.daemon.0.client.netmask_bits -v 29 ConfigPut
+
+sudo /usr/local/openvpn_as/scripts/sacli -u cmtsadmin -k access_to.0 -v "+ROUTE:$vnet" UserPropPut
+sudo /usr/local/openvpn_as/scripts/sacli -u cmtsadmin -k access_from.0 -v "+ALL_S2C_SUBNETS" UserPropPut
+
 
 
 sleep 2
 
-
-sudo systemctl stop openvpnas
-sudo systemctl start openvpnas
+/usr/local/openvpn_as/scripts/sacli stop
+/usr/local/openvpn_as/scripts/sacli start
