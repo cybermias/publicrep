8751c6e75be8d412c9f6b183acaa61e89bf18fad
nadavna <37038484+nadavna@users.noreply.github.com>
2019-07-10 11:49:19 +0300

    Update bridgeguac.sh

--- guacconf/bridgeguac.sh
+++ guacconf/bridgeguac.sh
@@ -4,7 +4,7 @@
 # Parameters come in triplets (vmName vmIP vmProt). Allowed protocols are RDP, RDPatlas, SSH, VNC and SSHctf [no user/pass].
 vnet="$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d. -f1-2).0.0/16"
 static="$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d. -f1-2).253.252"
-brnet="$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d. -f1-2).253.224"
+brnet="$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d. -f1-2).253.16"
 
 args=("$@")
 
