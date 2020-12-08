36428b2b926a04fd4c56075a557dac42d9b7ba83
nadavna <37038484+nadavna@users.noreply.github.com>
2020-12-08 11:29:36 +0200

    Update quasaratlas.ps1

--- microsoft/provisioning/quasaratlas.ps1
+++ microsoft/provisioning/quasaratlas.ps1
@@ -22,4 +22,6 @@ Invoke-WebRequest -uri $downloadlink -OutFile $localpath
 Start-Process powershell.exe -Credential $localcred -ArgumentList "start-process -filepath $localpath -verb runas"
 
 cscript c:\windows\system32\slmgr.vbs /rearm
+REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\" /v "PagingFiles" /t REG_MULTI_SZ /d "D:\pagefile.sys 0 0" /f
+
 shutdown /r /t 03
