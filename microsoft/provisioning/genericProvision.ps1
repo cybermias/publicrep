0f70ebb80d486c15c62dc83e59586ce3f775efd5
nadavna <37038484+nadavna@users.noreply.github.com>
2021-01-24 00:34:24 +0200

    Create genericProvision.ps1

---
+++ microsoft/provisioning/genericProvision.ps1
@@ -0,0 +1,36 @@
+### Basic non-Parameterized ps1 script to allow introduction of various limited-provisioning commands to Windows 10
+param(
+  [String]$defAdminUsr, 
+  [String]$defAdminPwd,
+  [String]$domain,
+  [String]$domAdminUsr,
+  [String]$domAdminPwd,
+  [String]$hostname
+)
+
+### Still missing in current build
+#   Condition the following functions: Domain add, Computer rename, PSCredential creation, RAT addons, Evaluation rearm
+#
+###
+Get-EventLog -LogName * | ForEach { Clear-EventLog $_.Log }
+
+# Define PSCredential variables following input from arguments (Domain and Local)
+$temppwd = ConvertTo-SecureString -String $defAdminPwd -AsPlainText -Force
+$localcred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $defAdminUsr,$temppwd
+
+$temppwd = ConvertTo-SecureString -String $domAdminPwd -AsPlainText -Force
+$domaincred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($domain + "\" + $domAdminUsr),$temppwd
+
+# Add the computer to a domain (if available)
+add-computer -domainname $domain -domaincredential $domaincred
+Add-LocalGroupMember -group "Remote Desktop Users" -member $domAdminUsr
+
+# Shift pagefile to the temporary drive (just in case)
+new-itemproperty -path "hklm:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -name PagingFiles -propertytype MultiString -value "D:\pagefile.sys" -force
+
+# Rename the computer according to the Arguments
+# For some reason rename-computer finishes with no errors, but it doesn't enforce
+rename-computer -newname $hostname -force -PassThru -ErrorAction Stop -DomainCredential $domaincred
+
+cscript c:\windows\system32\slmgr.vbs /rearm
+shutdown /r /t 03
