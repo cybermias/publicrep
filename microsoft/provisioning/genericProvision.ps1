aa7dec77b57c5ea2941c3c55a9caf71745126193
nadavna <37038484+nadavna@users.noreply.github.com>
2021-01-24 20:26:29 +0200

    Create genericProvision.ps1

---
+++ microsoft/provisioning/genericProvision.ps1
@@ -0,0 +1,93 @@
+### Basic non-Parameterized ps1 script to allow introduction of various limited-provisioning commands to Windows 10
+param(
+  [String]$state,
+  [String]$defAdminUsr, 
+  [String]$defAdminPwd,
+  [String]$domain,
+  [String]$domAdminUsr,
+  [String]$domAdminPwd,
+  [String]$hostname
+)
+
+### 
+#
+#
+#
+###
+
+
+if ($state -eq "init")
+{
+  # Define PSCredential variables following input from arguments (Domain and Local)
+  $temppwd = ConvertTo-SecureString -String $defAdminPwd -AsPlainText -Force
+  $localcred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $defAdminUsr,$temppwd
+
+  $temppwd = ConvertTo-SecureString -String $domAdminPwd -AsPlainText -Force
+  $domaincred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($domain + "\" + $domAdminUsr),$temppwd
+
+  # Force change computer name without a restart (rename computer including some registry changes)
+  #do {
+  #    $failed = $false
+  #    Try {
+  #        Write-Host "Renaming Computer.."
+  #        rename-computer -newname $hostname -force -PassThru -ErrorAction Stop
+  #    } catch { 
+  #        $failed = $true
+  #        Write-Host "Renaming Computer Failed, sleeping for 4 seconds.(Parameters: hostname: $hostname)"
+  #        Write-Output $_.Exception.Message
+  #        start-Sleep -Seconds 4
+  #    }
+  #} while ($failed)
+  
+  # Rename the computer
+  rename-computer -newname $hostname -force -PassThru
+  
+  # Shift pagefile to the temporary drive (just in case)
+  new-itemproperty -path "hklm:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -name PagingFiles -propertytype MultiString -value "D:\pagefile.sys" -force
+
+  # Fix evaluation license
+  cscript c:\windows\system32\slmgr.vbs /rearm
+  
+  $restartRequired = "yes"
+}
+
+
+if ($state -eq "domain")
+{
+  # Add computer to domain (after succesfully changing computer name *AND* init-extension restart
+  do {
+      $failed = $false
+      Try {
+          Write-Host "Adding Computer to Domain.."
+          add-computer -domainname $domain -domaincredential $domaincred -force -ErrorAction Stop 
+      } catch { 
+          $failed = $true 
+          Write-Host "Adding Computer to Domain failed, sleeping for 4 seconds.."
+          Write-Output $_.Exception.Message
+          start-Sleep -Seconds 5
+      }
+  } while ($failed)
+  
+  Add-LocalGroupMember -group "Remote Desktop Users" -member ($domain + "\Domain Users") | Out-Null
+  
+  $restartRequired = "yes"
+}
+
+
+# Repeating activtiy through any kind of initialization
+Get-EventLog -LogName * | ForEach { Clear-EventLog $_.Log }
+
+if ($restartRequired -eq "yes")
+{
+  shutdown /r /t 03
+}
+
+### TRASH / ARCHIVE
+#Remove-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -name "Hostname" 
+#Remove-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -name "NV Hostname" 
+#Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Computername\Computername" -name "Computername" -value $hostname
+#Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Computername\ActiveComputername" -name "Computername" -value $hostname
+#Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -name "Hostname" -value $hostname
+#Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -name "NV Hostname" -value  $hostname
+#Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -name "AltDefaultDomainName" -value $hostname
+#Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -name "DefaultDomainName" -value $hostname
