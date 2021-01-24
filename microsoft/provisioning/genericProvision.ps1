ad2126dffe511cf444f05153e6f26f31726ca81e
nadavna <37038484+nadavna@users.noreply.github.com>
2021-01-24 15:42:56 +0200

    Update genericProvision.ps1

--- microsoft/provisioning/genericProvision.ps1
+++ microsoft/provisioning/genericProvision.ps1
@@ -27,40 +27,70 @@ $domaincred = New-Object -TypeName System.Management.Automation.PSCredential -Ar
 new-itemproperty -path "hklm:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -name PagingFiles -propertytype MultiString -value "D:\pagefile.sys" -force
 
 # If computer already renamed, add to domain. Otherwise, rename it first (used in dual extension execution templates only!)
-if ($env:computername -eq $hostname)
-{
-  do {
-      $failed = $false
-      Try {
-          Write-Host "Adding Computer to Domain.."
-          add-computer -domainname $domain -domaincredential $domaincred -newname $hostname -ErrorAction Stop 
-      } catch { 
-          $failed = $true 
-          Write-Host "Adding Computer to Domain failed, sleeping for 4 seconds.."
-          Write-Output $_.Exception.Message
-          start-Sleep -Seconds 4
-      }
-  } while ($failed)
-  
-  # Make sure domain admins can log in via RDP
-  Add-LocalGroupMember -group "Remote Desktop Users" -member ($domain + "\Domain Admins") | Out-Null
-  shutdown /r /t 03
-}
-else
-{
-  do {
-      $failed = $false
-      Try {
-          Write-Host "Renaming Computer.."
-          rename-computer -newname $hostname -force -PassThru -ErrorAction Stop
-      } catch { 
-          $failed = $true
-          Write-Host "Renaming Computer Failed, sleeping for 4 seconds.(Parameters: hostname: $hostname)"
-          Write-Output $_.Exception.Message
-          start-Sleep -Seconds 4
-      }
-  } while ($failed)
-  cscript c:\windows\system32\slmgr.vbs /rearm
-  shutdown /r /t 03
-}
+#if ($env:computername -eq $hostname)
+#{
+#  do {
+#      $failed = $false
+#      Try {
+#          Write-Host "Adding Computer to Domain.."
+#          add-computer -domainname $domain -domaincredential $domaincred -newname $hostname -ErrorAction Stop 
+#      } catch { 
+#          $failed = $true 
+#          Write-Host "Adding Computer to Domain failed, sleeping for 4 seconds.."
+#          Write-Output $_.Exception.Message
+#          start-Sleep -Seconds 4
+#      }
+#  } while ($failed)
+# 
+#  # Make sure domain admins can log in via RDP
+#  Add-LocalGroupMember -group "Remote Desktop Users" -member ($domain + "\Domain Admins") | Out-Null
+#  shutdown /r /t 03
+#}
+#else
+#{
+#  do {
+#      $failed = $false
+#      Try {
+#          Write-Host "Renaming Computer.."
+#          rename-computer -newname $hostname -force -PassThru -ErrorAction Stop
+#      } catch { 
+#          $failed = $true
+#          Write-Host "Renaming Computer Failed, sleeping for 4 seconds.(Parameters: hostname: $hostname)"
+#          Write-Output $_.Exception.Message
+#          start-Sleep -Seconds 4
+#      }
+#  } while ($failed)
+#  cscript c:\windows\system32\slmgr.vbs /rearm
+#  shutdown /r /t 03
+#}
 
+do {
+    $failed = $false
+    Try {
+        Write-Host "Renaming Computer.."
+        rename-computer -newname $hostname -force -PassThru -ErrorAction Stop
+    } catch { 
+        $failed = $true
+        Write-Host "Renaming Computer Failed, sleeping for 4 seconds.(Parameters: hostname: $hostname)"
+        Write-Output $_.Exception.Message
+        start-Sleep -Seconds 4
+    }
+} while ($failed)
+
+start-Sleep -Seconds 5
+
+do {
+    $failed = $false
+    Try {
+        Write-Host "Adding Computer to Domain.."
+        add-computer -domainname $domain -domaincredential $domaincred -newname $hostname -Options JoinWithNewName,AccountCreate -ErrorAction Stop 
+    } catch { 
+        $failed = $true 
+        Write-Host "Adding Computer to Domain failed, sleeping for 4 seconds.."
+        Write-Output $_.Exception.Message
+        start-Sleep -Seconds 4
+    }
+} while ($failed)
+
+cscript c:\windows\system32\slmgr.vbs /rearm
+shutdown /r /t 03
