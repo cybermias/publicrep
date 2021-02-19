5f51811f3dd1dc5866f0aaa37f6d374a0cd4ae9f
nadavna <37038484+nadavna@users.noreply.github.com>
2021-02-20 00:44:02 +0200

    Create provisionWef.ps1

---
+++ microsoft/provisioning/wef/provisionWef.ps1
@@ -0,0 +1,18 @@
+<# 
+Whenever, whereever
+<#
+
+winrm quickconfig -q
+& sc.exe config WinRM start= auto
+
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Add-LocalGroupMember -Group "Event Log Readers" -Member $env:ComputerName
+
+Invoke-WebRequest -uri https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml -OutFile sysmonconfig-export.xml
+
+sysmon64.exe -accepteula -i sysmonconfig-export.xml 2>&1 | %{ "$_" }
+
+& wevtutil sl Microsoft-Windows-Sysmon/Operational /ca:"O:BAG:SYD:(A;;0xf0007;;;SY)(A;;0x7;;;BA)(A;;0x1;;;BO)(A;;0x1;;;SO)(A;;0x1;;;S-1-5-32-573)(A;;0x1;;;S-1-5-20)"
+
+restart-service WinRM
