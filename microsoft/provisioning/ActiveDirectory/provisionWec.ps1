5809567d6f6ba49fd1815a46138c3470ba54e7f0
nadavna <37038484+nadavna@users.noreply.github.com>
2021-02-20 01:34:41 +0200

    Update provisionWec.ps1

--- microsoft/provisioning/ActiveDirectory/provisionWec.ps1
+++ microsoft/provisioning/ActiveDirectory/provisionWec.ps1
@@ -26,7 +26,11 @@ winrm quickconfig -transport:http
 $collector = $env:ComputerName
 $fqdn = $collector+"."+$domain
 
-sysmon64.exe -accepteula -i 2>&1 | %{ "$_" }
+# Making sure the collector (AD) himself is reporting too
+Invoke-WebRequest -uri https://raw.githubusercontent.com/cybermias/publicrep/master/microsoft/provisioning/wef/sysmonconfig-export.xml -OutFile sysmonconfig-export.xml
+sysmon64.exe -accepteula -i sysmonconfig-export.xml 2>&1 | %{ "$_" }
+& wevtutil sl Microsoft-Windows-Sysmon/Operational /ca:"O:BAG:SYD:(A;;0xf0007;;;SY)(A;;0x7;;;BA)(A;;0x1;;;BO)(A;;0x1;;;SO)(A;;0x1;;;S-1-5-32-573)(A;;0x1;;;S-1-5-20)"
+#sysmon64.exe -accepteula -i 2>&1 | %{ "$_" }
 
 Invoke-WebRequest -uri https://raw.githubusercontent.com/cybermias/publicrep/master/microsoft/provisioning/sysmon/sysmon.xml -outfile sysmon.xml
 
