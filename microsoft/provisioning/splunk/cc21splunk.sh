80dcb12dc5c4a046558e1f168d8f0386d6f3bd09
nadavna <37038484+nadavna@users.noreply.github.com>
2021-02-20 17:26:19 +0200

    Update cc21splunk.sh

--- microsoft/provisioning/splunk/cc21splunk.sh
+++ microsoft/provisioning/splunk/cc21splunk.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 export SPLUNK_HOME=/opt/splunk
 
-echo '[wef]
+
+
+sudo mkdir /opt/splunk/etc/apps/inputs_wef
+sudo mkdir /opt/splunk/etc/apps/inputs_wef/local
+
+
+sudo echo '[wef]
 homePath = $SPLUNK_DB/wefdb/db
 coldPath = $SPLUNK_DB/wefdb/colddb
 thawedPath = $SPLUNK_DB/wefdb/thaweddb' >> /opt/splunk/etc/system/local/indexes.conf
 
-echo '[udp://:10514]
+sudo echo '[udp://:10514]
 index=wef
 sourcetype=wef
-disabled = 0' >> /opt/splunk/etc/apps/inputs_pf/local/inputs.conf
+disabled = 0' >> /opt/splunk/etc/apps/inputs_wef/local/inputs.conf
 
 
