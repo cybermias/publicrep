1c34b9fcda5450da8829d792eb768aa7ebc6130e
nadavna <37038484+nadavna@users.noreply.github.com>
2021-02-20 22:01:53 +0200

    Update cc21splunk.sh

--- microsoft/provisioning/splunk/cc21splunk.sh
+++ microsoft/provisioning/splunk/cc21splunk.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 export SPLUNK_HOME=/opt/splunk
 
-
-
 sudo mkdir /opt/splunk/etc/apps/inputs_wef
 sudo mkdir /opt/splunk/etc/apps/inputs_wef/local
 
+## Fixing the snapshot of splunk 812 (already containing pf configus on "fw" - but trippled in indexes.conf)
+## We remove the current indexes.conf first and fix it with only one [fw]. The app was already installed before snapshot.
+## Notice we dont delete the file but append it with '>'.
+
+sudo echo '[fw]
+homePath = $SPLUNK_DB/fwdb/db
+coldPath = $SPLUNK_DB/fwdb/colddb
+thawedPath = $SPLUNK_DB/fwdb/thaweddb' > /opt/splunk/etc/system/local/indexes.conf
 
 sudo echo '[wef]
 homePath = $SPLUNK_DB/wefdb/db
@@ -14,7 +20,7 @@ thawedPath = $SPLUNK_DB/wefdb/thaweddb' >> /opt/splunk/etc/system/local/indexes.
 
 sudo echo '[udp://:10514]
 index=wef
-sourcetype=wef
+sourcetype=windows_snare_syslog
 disabled = 0' >> /opt/splunk/etc/apps/inputs_wef/local/inputs.conf
 
 
