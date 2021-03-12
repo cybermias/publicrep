1fef9a57434b209f3be1a6b8498f84a03f26476b
nadavna <37038484+nadavna@users.noreply.github.com>
2021-03-12 17:05:19 +0200

    Update ElasticFix.sh

--- microsoft/provisioning/AtlasLab/Elastic/ElasticFix.sh
+++ microsoft/provisioning/AtlasLab/Elastic/ElasticFix.sh
@@ -3,3 +3,5 @@
 sudo sed -i 's/\"localhost\"/\"0\"/g' /etc/elasticsearch/elasticsearch.yml
 sudo bash -c 'echo "discovery.type: single-node" >> /etc/elasticsearch/elasticsearch.yml'
 sudo service elasticsearch restart
+
+sudo bash -c 'echo "nameserver 10.200.11.200" >> /etc/resolv.conf'
