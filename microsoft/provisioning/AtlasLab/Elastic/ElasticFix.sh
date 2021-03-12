01e36f33271d8c3fba7f9cad70efda7ba647d231
nadavna <37038484+nadavna@users.noreply.github.com>
2021-03-12 21:03:30 +0200

    Update ElasticFix.sh

--- microsoft/provisioning/AtlasLab/Elastic/ElasticFix.sh
+++ microsoft/provisioning/AtlasLab/Elastic/ElasticFix.sh
@@ -62,8 +62,8 @@ sudo sed -i "s/FiOS/UNTRUST/g" /etc/pfelk/conf.d/20-interfaces.conf
 sudo sed -i "s/Home Network/TRUST/g" /etc/pfelk/conf.d/20-interfaces.conf
 sudo systemctl start elasticsearch 
 sudo systemctl start kibana 
-sudo /bin/systemctl daemon-reload
-sudo systemctl enable logstash.service
+sudo systemctl daemon-reload
+sudo systemctl enable logstash
 sudo systemctl start logstash
 
 ## Add Dashboards and Templates for pfelk
