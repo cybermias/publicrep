cf87af512de7bbbc13b6437050bcf91309df3a99
nadavna <37038484+nadavna@users.noreply.github.com>
2021-03-12 19:58:47 +0200

    Update AtlasAD.ps1

--- microsoft/provisioning/AtlasLab/Elastic/AtlasAD.ps1
+++ microsoft/provisioning/AtlasLab/Elastic/AtlasAD.ps1
@@ -66,7 +66,7 @@ if ($deploy -eq "splunk")
 ## Installing Elastics WingLogBeat
 if ($deploy -eq "elastic")
 {
-	Start-Process choco 'install -y notepadplusplus 7zip'
+	#Start-Process choco 'install -y notepadplusplus 7zip'
 	Set-ExecutionPolicy -ExecutionPolicy Unrestricted -scope process -force
 	$winlogbeatUri = "https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-7.11.2-windows-x86_64.zip"
 	$winlogbeatZip = "winlogbeat.zip"
