7b371c69e4135ea1e21f5ca95b84d7c9b8a9f090
nadavna <37038484+nadavna@users.noreply.github.com>
2021-03-12 21:28:52 +0200

    Update AtlasAD.ps1

--- microsoft/provisioning/AtlasLab/Elastic/AtlasAD.ps1
+++ microsoft/provisioning/AtlasLab/Elastic/AtlasAD.ps1
@@ -83,7 +83,7 @@ if ($deploy -eq "elastic")
 	((Get-Content -path $winlogbeatYml -Raw) -replace '#host:','host:') | Set-Content -Path $winlogbeatYml
 	
 	# Give Elastic (pfelk) time to breath (<==verify this matter)
-	start-sleep -s 2 
+	start-sleep -s 6 
 	start-process winlogbeat.exe 'setup -e' -wait
 	Start-Service winlogbeat
 	# Assign Elastic with DNS record (future: automate the hostname)
