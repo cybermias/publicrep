67fcb8303cd018b2787a9d6077dfd4930707fc5a
nadavna <37038484+nadavna@users.noreply.github.com>
2021-03-13 18:23:32 +0200

    Update ElasticFix.sh

--- microsoft/provisioning/AtlasLab/Elastic/ElasticFix.sh
+++ microsoft/provisioning/AtlasLab/Elastic/ElasticFix.sh
@@ -2,15 +2,26 @@
 
 ## Comments
 # This script must be rewritten with automation for "AD" (as DNS) ip address and pfSense LAN interface ip address
-# Additional automation is required for the "AD" (as NTP Sync) 
+# Additional automation is required for the "AD" (as NTP Sync) .
+#
+# UDP SYSLOG (from pfsense) is fixated at 20514 [sed'ing @PORT@ from 01-inputs.conf. Requires Input from template file
+#
 
+## Changing some elastic configurations
+# Fixing yml for "localhost" and adding a non-cluster parameter required to work properly
 sudo sed -i 's/\"localhost\"/\"0.0.0.0\"/g' /etc/elasticsearch/elasticsearch.yml
 sudo bash -c 'echo "discovery.type: single-node" >> /etc/elasticsearch/elasticsearch.yml'
+sudo systemctl restart elasticsearch 
 
-# DNS
+# DNS nameserver addition (for the AD, requires automation through parameter)
 sudo bash -c 'echo "nameserver 10.200.11.200" >> /etc/resolv.conf'
-# NTP
+# NTP addition (for the AD, requires automation through parameter)
 sudo bash -c 'echo "Servers=10.200.11.200" >> /etc/systemd/timesyncd.conf'
+
+## PFELK configruations. Not the original instructions as they include from malconfigured declarations
+## 1) pfelk_installer.sh requires human intervention, initial attempts at bypassing this requires too much efforts with minimal success
+## 2) Reverting to making changes according to manual installation instructions.
+## 3) Some configurations required extra efforts to avoid unknown conflicts post deployments (index / pattern may not appear)
 
 #configure logstash for pfSense (with pfelk https://github.com/pfelk/pfelk/blob/main/install/ubuntu.md)
 sudo mkdir -p /etc/pfelk/{conf.d,config,logs,databases,patterns,scripts,templates}
@@ -38,43 +49,52 @@ sudo cat <<EOF > /etc/pfelk/conf.d/50-outputs.conf
   }
 EOF
 
-# Extra pfelk (not all configured)
+# Making some adaptations to the inputs file (clearing most of the non-currently-used techs: Suricata, haproxy, etc.
+sudo cat <<EOF > /etc/pfelk/conf.d/01-inputs.conf
+input {
+  udp {
+    id => "pfelk-2" 
+    type => "firewall-2"
+    port => @PORT@
+  }
+}
+EOF
+
+# Removed pfelk confs that were either not used (or had no value in the initial pfelk installation - apperantly not used too)
 #sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/conf.d/35-rules-desc.conf -P /etc/pfelk/conf.d/
 #sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/conf.d/36-ports-desc.conf -P /etc/pfelk/conf.d/
 #sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/conf.d/37-enhanced_user_agent.conf -P /etc/pfelk/conf.d/
 #sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/conf.d/38-enhanced_url.conf -P /etc/pfelk/conf.d/
+#sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/databases/private-hostnames.csv -P /etc/pfelk/databases/
+#sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/databases/rule-names.csv -P /etc/pfelk/databases/
+#sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/databases/service-names-port-numbers.csv -P /etc/pfelk/databases/
+
+# Replacing the syslog port (as configured in pfsense) <== Requires parameter automation
+sudo sed -i "s/@PORT@/20514/g" /etc/pfelk/conf.d/01-inputs.conf
+
+# Additional error-collecting script from current pfelk
+sudo wget -q https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/scripts/error-data.sh -P /etc/pfelk/scripts/
+sudo chmod +x /etc/pfelk/scripts/error-data.sh
+sleep 4
+
+# Adding the grok pattern offered by current pfelk
+sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/patterns/pfelk.grok -P /etc/pfelk/patterns/
+sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/patterns/openvpn.grok -P /etc/pfelk/patterns/
 
 
+## Leftovers from other pfelk version - requiring the pfsense IP. Not used for currently implementation.
 # Requires automation planning - currently not allowing other pfsense/gw ip's than X.Y.0.254
 #vnet="$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d. -f1)"
 #trustnet="$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d. -f2)"
 #sudo sed -i "s/\/10\\.0\\.0\\.1\//\/$vnet\\.$trustnet\\.0\\.254\//g" /etc/logstash/conf.d/10-syslog.conf
 
-# Simplifying the syslog (not to change pfsense provisoning scripts for this)
-sudo sed -i "s/5140/20514/g" /etc/pfelk/conf.d/01-inputs.conf
-
-sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/patterns/pfelk.grok -P /etc/pfelk/patterns/
-sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/patterns/openvpn.grok -P /etc/pfelk/patterns/
-#sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/databases/private-hostnames.csv -P /etc/pfelk/databases/
-#sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/databases/rule-names.csv -P /etc/pfelk/databases/
-#sudo wget https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/databases/service-names-port-numbers.csv -P /etc/pfelk/databases/
-
-sudo wget -q https://raw.githubusercontent.com/pfelk/pfelk/main/etc/pfelk/scripts/error-data.sh -P /etc/pfelk/scripts/
-sudo chmod +x /etc/pfelk/scripts/error-data.sh
-
-sleep 4
-
-## Amend pfsense interfaces (NOT AUTOMATED!)
+# Amend pfsense interfaces (NOT AUTOMATED!) - currently not used until further investigations
 #sudo sed -i "s/igb0/hn0/g" /etc/pfelk/conf.d/20-interfaces.conf
 #sudo sed -i "s/igb1/hn1/g" /etc/pfelk/conf.d/20-interfaces.conf
 #sudo sed -i "s/FiOS/UNTRUST/g" /etc/pfelk/conf.d/20-interfaces.conf
 #sudo sed -i "s/Home Network/TRUST/g" /etc/pfelk/conf.d/20-interfaces.conf
 
 ## Get everything up and running
-sudo systemctl restart elasticsearch 
-sudo systemctl restart kibana 
-sudo systemctl enable logstash
-
 sleep 5
 
 ## Add Dashboards and Templates for pfelk
@@ -124,4 +144,5 @@ else
   fi
 fi
 
+sudo systemctl enable logstash
 sudo systemctl start logstash
