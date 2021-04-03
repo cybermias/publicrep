e5c81e831bb1ef768a62ddcade98ddc03a4a664f
nadavna <37038484+nadavna@users.noreply.github.com>
2021-04-04 02:23:55 +0300

    Update genericProvision.ps1

--- microsoft/provisioning/genericProvision.ps1
+++ microsoft/provisioning/genericProvision.ps1
@@ -34,10 +34,10 @@ param(
 Set-TimeZone -Id "Middle East Standard Time"
 # Before anything, get rid of OneDrive!
 if (Test-Path "$env:systemroot\System32\OneDriveSetup.exe") {
-    "$env:systemroot\System32\OneDriveSetup.exe" /uninstall
+    c:\windows\System32\OneDriveSetup.exe /uninstall
 }
 if (Test-Path "$env:systemroot\SysWOW64\OneDriveSetup.exe") {
-    "$env:systemroot\SysWOW64\OneDriveSetup.exe" /uninstall
+    c:\windows\SysWOW64\OneDriveSetup.exe /uninstall
 }
   
 # Define PSCredential variables following input from arguments (Domain and Local)
