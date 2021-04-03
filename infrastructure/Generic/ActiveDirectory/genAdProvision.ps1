59856125f8150a3b81a3624fe7d51cb23fe2ba69
nadavna <37038484+nadavna@users.noreply.github.com>
2021-04-04 00:01:45 +0300

    Update genAdProvision.ps1

--- infrastructure/Generic/ActiveDirectory/genAdProvision.ps1
+++ infrastructure/Generic/ActiveDirectory/genAdProvision.ps1
@@ -41,7 +41,8 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 $genCompOu = "ComputersOU"
 New-ADOrganizationalUnit -Name $genCompOu
 $dc = $domain.split(".")
-redircmp "OU=$($genCompOu),DC=$($dc[0]),DC=$($dc[1])"
+$OU = "OU=$($genCompOu),DC=$($dc[0]),DC=$($dc[1])"
+redircmp $OU
 
 # Preparing GPO's for stuff (not altering FW on workstations yet allowing domain controller to GP updates). Adding also winrm
 New-GPO -Name "Remote Management Automation" -StarterGpoName "Group Policy Remote Update Firewall Ports" | New-GPLink -Target "OU=$($genCompOu),DC=$($dc[0]),DC=$($dc[1])" -LinkEnabled yes
@@ -68,6 +69,71 @@ else {
 # If CSV isn't provided - nothing happens. Update with future manual parameters.
 }
 
+
+### PREVIEW - TEST - Forcing WINRM/PS-REMOTING via GPO
+### Gracefuly adopted from: https://powertoe.wordpress.com/2011/05/16/enable-winrm-with-group-policy-but-use-powershell-to-create-the-policy/
+# Create the GPO
+$gpo = New-GPO Remoting -Comment 'GPO that will enable remoting'
+
+# Add the policy to allow WinRM
+$winrmkey = 'HKLM\Software\Policies\Microsoft\Windows\WinRM\Service'
+$params = @{
+    Key = $winrmkey;
+    ValueName = 'AllowAutoConfig';
+    Value = 1;
+    Type = 'Dword';
+}
+$gpo |Set-GPRegistryValue @params
+
+# Set the filters to allow IPv4 and IPv6 traffic from all IPs for WinRM
+$winrmkey = 'HKLM\Software\Policies\Microsoft\Windows\WinRM\Service'
+$params = @{
+    Key = $winrmkey;
+    ValueName = 'IPv4Filter';
+    Value = '*';
+    Type = 'String';
+}
+$gpo |Set-GPRegistryValue @params
+$params.ValueName = 'IPv6Filter'
+$gpo |Set-GPRegistryValue @params
+
+# Add the firewall rule to allow port 5985 for WinRM HTTP traffic
+$fwrule = 'v2.10|Action=Allow|Active=TRUE|Dir=In|Protocol=6|LPort=5985|'
+'App=System|Name=@FirewallAPI.dll,-30253|Desc=@FirewallAPI.dll'
+$fwrule += ',-30256|EmbedCtxt=@FirewallAPI.dll,-30252'
+
+$params = @{
+    Key = 'HKLM\Software\Policies\Microsoft\WindowsFirewall\FirewallRules';    
+    ValueName = 'WINRM-HTTP-In-TCP';
+    Value = $fwrule;
+    Type = 'String';
+}
+$gpo |Set-GPRegistryValue @params
+
+# Manually add the inf setting to configure WinRM to start automatically
+$inf = @'
+[Unicode]
+Unicode=yes
+[Version]
+signature="$CHICAGO$"
+Revision=1
+[Service General Setting]
+"WinRM",2,"D:AR"
+
+'@
+$sdsad = [System.DirectoryServices.ActiveDirectory.Domain]
+$domain = $sdsad::GetCurrentDomain().name
+$path = "\\$domain\sysvol\$($env:LOGONSERVER)\Policies\$($gpo.Id)\Machine\"
+$path += "Microsoft\Windows NT\SecEdit"
+if (!(Test-Path $path)) {
+    md $path
+}
+$inf |Out-File (Join-Path $path 'GptTmpl.inf')
+# Link the GPO to the OU
+$link = $gpo |
+    New-GPLink -Target $OU -LinkEnabled 'Yes'
+
+### END OF PREVIEW FEATURE
 
 # Clear some Azure Crap (removed due to "protectedSettings" - will review)
 #Remove-Item 'C:\WindowsAzure\Logs\Plugins','C:\WindowsAzure\Logs\AggregateStatus','C:\WindowsAzure\CollectGuestLogsTemp','C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension' -Force -Confirm:$False -recurse | out-null
