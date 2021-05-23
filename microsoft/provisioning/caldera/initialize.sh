8feda6a24de9eaa6610e159d9bbeb2869d51b31c
nadavna <37038484+nadavna@users.noreply.github.com>
2021-05-23 18:02:50 +0300

    Update initialize.sh

--- microsoft/provisioning/caldera/initialize.sh
+++ microsoft/provisioning/caldera/initialize.sh
@@ -5,8 +5,11 @@
 
 # Adapt the local.yml file at conf directory to initialize caldera as the current local IP
 ip="$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d. -f1-4 | cut -d/ -f1)"
-sed -i -e "s/0.0.0.0/$ip/g" /home/cmtsadmin/caldera/conf/local.yml
+sudo sed -i -e "s/0.0.0.0/$ip/g" /home/cmtsadmin/caldera/conf/local.yml
 
-kill -9 $(pgrep -f 'server.py')
-cd /home/cmtsadmin/caldera
-/usr/bin/python3 server.py
+sudo kill -9 $(pgrep -f 'server.py')
+
+sudo reboot now
+
+#cd /home/cmtsadmin/caldera
+#/usr/bin/python3 server.py
