df22725f45f31230bade2186c07aa7b2c5858ba7
nadavna <37038484+nadavna@users.noreply.github.com>
2021-05-23 16:08:38 +0300

    Update initialize.sh

--- microsoft/provisioning/caldera/initialize.sh
+++ microsoft/provisioning/caldera/initialize.sh
@@ -5,10 +5,9 @@
 
 apt-get update
 pip3 install --upgrade setuptools
-cd /opt/caldera
 
 # Adapt the local.yml file at conf directory to initialize caldera as the current local IP
 ip=$(ifconfig eth0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-sed -i -e "s/0.0.0.0/$ip/g" /opt/caldera/conf/local.yml
+sed -i -e "s/0.0.0.0/$ip/g" $HOME/caldera/conf/local.yml
 
 service caldera restart
