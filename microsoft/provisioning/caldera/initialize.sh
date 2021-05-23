6ab7affac4a0b8922164e4ea8ef25d692872eb7d
nadavna <37038484+nadavna@users.noreply.github.com>
2021-05-23 16:16:42 +0300

    Update initialize.sh

--- microsoft/provisioning/caldera/initialize.sh
+++ microsoft/provisioning/caldera/initialize.sh
@@ -4,10 +4,9 @@
 # Used with snapshot running golang1.16 and caldera git 3.1.0
 
 apt-get update
-pip3 install --upgrade setuptools
 
 # Adapt the local.yml file at conf directory to initialize caldera as the current local IP
 ip=$(ifconfig eth0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 sed -i -e "s/0.0.0.0/$ip/g" $HOME/caldera/conf/local.yml
 
-service caldera restart
+sudo reboot now
