36a73758442c0b196f70b84199efa504a726ca3e
nadavna <37038484+nadavna@users.noreply.github.com>
2021-05-23 21:08:42 +0300

    Update initialize.sh

--- microsoft/provisioning/caldera/initialize.sh
+++ microsoft/provisioning/caldera/initialize.sh
@@ -8,6 +8,9 @@ apt-get install net-tools
 ip="$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d. -f1-4 | cut -d/ -f1)"
 sed -i -e "s/0.0.0.0/$ip/g" /home/cmtsadmin/caldera/conf/local.yml
 
+# Manx requires websocket to work back to caldera operator browser (since it can be through iptables of openvpn, we reserve the 0.0.0.0:7012 in that line only):
+sed -i -e "s/app.contact.websocket:.*/app.contact.websocket: 0\.0\.0\.0:7012/g" /home/cmtsadmin/caldera/conf/local.yml
+
 kill -9 $(pgrep -f 'server.py')
 
 
