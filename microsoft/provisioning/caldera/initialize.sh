49bc4cd0df42299a16c6e9cd573dd54a32ac9527
nadavna <37038484+nadavna@users.noreply.github.com>
2021-05-23 03:11:02 +0300

    Update initialize.sh

--- microsoft/provisioning/caldera/initialize.sh
+++ microsoft/provisioning/caldera/initialize.sh
@@ -16,3 +16,10 @@ ip=$(ifconfig eth0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]
 sed -i -e "s/0.0.0.0/$ip/g" /opt/caldera/conf/local.yml
 
 service caldera restart
+
+sudo apt-get install -y software-properties-common
+sudo apt-get purge golang-go
+sudo rm -rvf /usr/local/go/
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt update
+sudo apt install -y golang-go
