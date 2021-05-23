e88ceb27ee5e41e7df093e3ebeec673c3ff64344
nadavna <37038484+nadavna@users.noreply.github.com>
2021-05-23 17:13:59 +0300

    Update initialize.sh

--- microsoft/provisioning/caldera/initialize.sh
+++ microsoft/provisioning/caldera/initialize.sh
@@ -3,8 +3,8 @@
 # Script will update caldera by git, and enforce it to run locally inside ATLAS lab (i.e 10.200.11.100).
 # Used with snapshot running golang1.16 and caldera git 3.1.0
 
-apt-get update
-apt-get install -y net-tools
+sudo apt update && sudo apt install -y python-is-python2
+sudo apt-get install -y net-tools
 
 # Adapt the local.yml file at conf directory to initialize caldera as the current local IP
 ip=$(ifconfig eth0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
