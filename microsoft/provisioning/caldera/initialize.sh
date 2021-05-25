fe64b91c406325ba1cb3af0433d93eb7a28fad8e
nadavna <37038484+nadavna@users.noreply.github.com>
2021-05-26 00:39:17 +0300

    Update initialize.sh

--- microsoft/provisioning/caldera/initialize.sh
+++ microsoft/provisioning/caldera/initialize.sh
@@ -12,6 +12,16 @@ apt-get install net-tools
 # Manx requires websocket to work back to caldera operator browser (since it can be through iptables of openvpn, we reserve the 0.0.0.0:7012 in that line only):
 #sed -i -e "s/app.contact.websocket:.*/app.contact.websocket: 0\.0\.0\.0:7012/g" /home/cmtsadmin/caldera/conf/local.yml
 
+sudo apt-get uninstall purge golang*
+sudo apt-get update
+sudo rm -rf /usr/local/go
+sudo apt install -y golang-go upx
+crontab -u cmtsadmin  -r
+
+#echo "GOPATH=$HOME/go" > cronstuff.txt
+#echo "PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> cronstuff.txt
+echo "@reboot (cd /home/cmtsadmin/caldera && /usr/bin/python3 server.py &)  > /tmp/cronCaldera.log 2>&1" >> cronstuff.txt
+crontab -u cmtsadmin cronstuff.txt
 
 # // # Adapt the local.yml file at conf directory to initialize caldera as the current external IP
 PUBLICIP=$(curl -s ipecho.net/plain)
@@ -24,6 +34,10 @@ sed -i -e "s/app.contact.tcp:.*/app.contact.tcp: $PUBLICIP:7010/g"  /home/cmtsad
 sed -i -e "s/app.contact.udp:.*/app.contact.udp: $PUBLICIP:7011/g"  /home/cmtsadmin/caldera/conf/local.yml
 sed -i -e "s/app.contact.websocket:.*/app.contact.websocket: $PUBLICIP:7012/g"  /home/cmtsadmin/caldera/conf/local.yml
 sed -i -e "s/app.contact.dns.domain:.*/app.contact.dns.domain: $1/g"  /home/cmtsadmin/caldera/conf/local.yml
+sed -i -e "s/reports_dir:.*/reports_dir: \/home\/cmtsadmin\/reports/g"  /home/cmtsadmin/caldera/conf/local.yml
+sed -i -e "s/exfil_dir:.*/exfil_dir: \/home\/cmtsadmin\/exfil/g"  /home/cmtsadmin/caldera/conf/local.yml
+
+
 
 # Make sure the caldera OS is aware of its public IP (otherwise errors are shown and stability wasn't examined when server.py is run - cannot bind "public IP")
 sudo cat <<EOF > /etc/netplan/60-static.yaml
@@ -35,6 +49,28 @@ network:
                 - $PUBLICIP/32
 EOF
 sudo netplan apply
+
+cd /home/cmtsadmin/caldera
+git checkout master
+git pull
+pip3 install -r requirements.txt
+pip3 install pyhuman
+        
+cd plugins/sandcat/gocat
+git checkout master
+git pull
+
+cd /home/cmtsadmin/caldera
+# Oh god do'nt ask why it's done again. Seriously. Veins will be CUT!
+pip3 install -r requirements.txt
+git checkout master
+git pull
+
+# AND AGAIN
+cd plugins/human
+pip3 install -r requirements.txt
+cd pyhuman
+pip3 install -r requirements.txt
 
 echo "script completed" > /home/cmtsadmin/finished
 shutdown -r 1
