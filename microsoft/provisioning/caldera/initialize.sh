07baa5984438b4cb40f599df958db11c7b4ce019
nadavna <37038484+nadavna@users.noreply.github.com>
2021-05-25 17:21:22 +0300

    Update initialize.sh

--- microsoft/provisioning/caldera/initialize.sh
+++ microsoft/provisioning/caldera/initialize.sh
@@ -4,19 +4,24 @@
 # Used with snapshot running golang1.16 and caldera git 3.1.0
 
 apt-get install net-tools
-# Adapt the local.yml file at conf directory to initialize caldera as the current local IP
-ip="$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d. -f1-4 | cut -d/ -f1)"
-sed -i -e "s/0.0.0.0/$ip/g" /home/cmtsadmin/caldera/conf/local.yml
-
+# /// # Adapt the local.yml file at conf directory to initialize caldera as the current local IP
+# When this was used internally, the upcoming lines will extract the internal IP. The current script version will refer to the external IP.
+#ip="$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d. -f1-4 | cut -d/ -f1)"
+#sed -i -e "s/0.0.0.0/$ip/g" /home/cmtsadmin/caldera/conf/local.yml
+# // # The following commands will only be relevant in an internal caldera deployment (and are currently DISABLED)
 # Manx requires websocket to work back to caldera operator browser (since it can be through iptables of openvpn, we reserve the 0.0.0.0:7012 in that line only):
-sed -i -e "s/app.contact.websocket:.*/app.contact.websocket: 0\.0\.0\.0:7012/g" /home/cmtsadmin/caldera/conf/local.yml
+#sed -i -e "s/app.contact.websocket:.*/app.contact.websocket: 0\.0\.0\.0:7012/g" /home/cmtsadmin/caldera/conf/local.yml
 
-kill -9 $(pgrep -f 'server.py')
 
+# // # Adapt the local.yml file at conf directory to initialize caldera as the current external IP
+PUBLICIP=$(curl -s ipecho.net/plain)
+while [ ! $PUBLICIP ]; do
+        PUBLICIP=$(curl -s ipecho.net/plain)
+done
+sed -i -e "s/0.0.0.0/$PUBLICIP/g" /home/cmtsadmin/caldera/conf/local.yml
 
 echo "script completed" > /home/cmtsadmin/finished
 shutdown -r 1
-
 
 
 #cd /home/cmtsadmin/caldera
